de996e5ae6e29abb00fb0cd2905055408ff1e4dd
Christine Nyambwari <159689583+stine-ri@users.noreply.github.com>
2024-07-23 11:22:27 +0300

    Delete dist directory

--- dist/CustomerSupportTickets/validator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CustomerSupportTicketsSchema = void 0;
-const zod_1 = require("zod");
-exports.CustomerSupportTicketsSchema = zod_1.z.object({
-    subject: zod_1.z.string(),
-    description: zod_1.z.string(),
-    status: zod_1.z.string(),
-});
--- dist/Locations/validator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LocationsSchema = void 0;
-const zod_1 = require("zod");
-exports.LocationsSchema = zod_1.z.object({
-    "name": zod_1.z.string(),
-    "address": zod_1.z.string(),
-    "contact_phone": zod_1.z.string(),
-});
